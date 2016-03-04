9eec277ba2864652a05d89388b1051f33ce27193
Fai Wu <faihuiwufw5@outlook.com>
2016-03-03 23:24:25 -0500

    first commit

---
+++ Meme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme
+//
+//  Created by Fai Wu on 2/21/16.
+//  Copyright © 2016 Fai Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
